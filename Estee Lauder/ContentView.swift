0d2325fd62561c2748e467c18da97ee3b0242a8a
ak9391 <ak9391@nyu.edu>
2023-07-27 21:00:57 +0530

    Main page animation

--- Estee Lauder/ContentView.swift
+++ Estee Lauder/ContentView.swift
@@ -7,20 +7,57 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+enum AnimationState {
+    case compress
+    case expand
+    case normal
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ContentView: View {
+    
+    @State var animate: Bool = false
+    @State var showSplash: Bool = true
+    
+    var body: some View {
+        VStack {
+            ZStack {
+                ZStack {
+                    VStack{
+                        Image("estee1")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    }
+                }
+                
+                ZStack {
+                    Image("Logo")
+                        .resizable()
+                        .ignoresSafeArea()
+                        .scaleEffect(animate ? 50 : 1)
+                        .animation(Animation.easeIn(duration: 0.8))
+                }
+                .animation(Animation.linear(duration: 0.5))
+                .animation(.default)
+                .opacity(showSplash ? 1 : 0)
+            }
+        }
+        .onAppear{
+            
+            DispatchQueue.main.asyncAfter(deadline: .now()+0.3){
+                animate.toggle()
+            }
+            
+            DispatchQueue.main.asyncAfter(deadline: .now()+1){
+                showSplash.toggle()
+            }
+
+        }
     }
 }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+

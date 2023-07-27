7eebc4a89f25ce817abf1ef3926df3b4895e00ad
ak9391 <ak9391@nyu.edu>
2023-07-27 19:26:35 +0530

    Initial Commit

---
+++ Estee Lauder/Estee_LauderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Estee_LauderApp.swift
+//  Estee Lauder
+//
+//  Created by Ashrita Kantamneni on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Estee_LauderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
